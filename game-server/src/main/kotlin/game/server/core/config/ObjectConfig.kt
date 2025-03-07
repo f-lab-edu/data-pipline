7d9adb99dce4aced2104c966c6b0ebbad54ee8e4
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-03-07 14:43:54 +0900

    UDPATE - objectMapper 직렬화 설정 - unknown 필드 무시하도록 설정 - LocalDateTime API 역/직렬화 가능하도록 설정 - 날짜 데이터 ISO 형식으로 출력하도록 설정 - json 직렬화에 클래스 타입 정보 추가하여 다형성 역/직렬화 가능하도록 설정

--- game-server/src/main/kotlin/game/server/core/config/ObjectConfig.kt
+++ game-server/src/main/kotlin/game/server/core/config/ObjectConfig.kt
@@ -1,6 +1,10 @@
 package game.server.core.config
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,6 +15,12 @@ open class ObjectConfig {
 
     @Bean
     open fun objectMapper(): ObjectMapper {
-        return ObjectMapper().registerKotlinModule()
+        return ObjectMapper().apply {
+            registerKotlinModule()
+            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            registerModule(JavaTimeModule())
+            disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS)
+            activateDefaultTyping(polymorphicTypeValidator, ObjectMapper.DefaultTyping.EVERYTHING, JsonTypeInfo.As.PROPERTY)
+        }
     }
 }
