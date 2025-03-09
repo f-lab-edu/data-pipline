c934121beb7e889e4158ee281ff51e60aee8e2e5
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-03-09 20:06:15 +0900

    update - 테스트 환경과 분리 위한 Profile 지정

--- game-server/src/main/kotlin/game/server/core/config/WebSocketConfig.kt
+++ game-server/src/main/kotlin/game/server/core/config/WebSocketConfig.kt
@@ -3,11 +3,13 @@ package game.server.core.config
 import game.server.core.controller.WebSocketController
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter
 
 @Configuration
+@Profile("local", "test-game")
 open class WebSocketConfig : WebFluxConfigurer {
 
     @Bean
