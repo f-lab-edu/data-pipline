13d70ef66e6f45552eb07f183a78a8fd7b39e922
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-03-16 15:12:18 +0900

    add - core 모듈 의존성 추가

--- game-server/build.gradle.kts
+++ game-server/build.gradle.kts
@@ -6,31 +6,15 @@ plugins {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-
-    // Serialize kotlin
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
-
-    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+    implementation(project(":core"))
 
     // oauth
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-    // coroutine
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.0")
-
     // database
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("com.github.jasync-sql:jasync-r2dbc-mysql:2.2.4")
-
-    // kafka
-    implementation("org.springframework.kafka:spring-kafka")
-    implementation("io.projectreactor.kafka:reactor-kafka:1.3.22")
-
-    // redis
-    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage")
