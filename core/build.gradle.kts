d685690999c65c5522237bc78daa5f8bbbda6271
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-03-16 20:54:38 +0900

    update - 게임 및 로비 모듈 의존성 위치 수정

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,30 +1,19 @@
 version = "1.0.0"
 
 plugins {
-    id("org.springframework.boot") version "3.1.4"
-    id("io.spring.dependency-management")
+    `java-library`
 }
 
 dependencies {
+    api(platform("org.springframework.boot:spring-boot-dependencies:3.1.4"))
+
+    // WebFlux, Validation 등 프레임워크 관련 의존성이지만 core 모듈에서는 공통 API 역할로만 제공합니다.
     api("org.springframework.boot:spring-boot-starter-webflux")
     api("org.springframework.boot:spring-boot-starter-validation")
 
-    // Serialize kotlin
+    // Jackson Kotlin 모듈 (역/직렬화)
     api("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
 
-    api("net.logstash.logback:logstash-logback-encoder:7.4")
-
-    // coroutine
+    // 코루틴과 Reactor 연동
     api("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.0")
-
-    // kafka
-    api("org.springframework.kafka:spring-kafka")
-    api("io.projectreactor.kafka:reactor-kafka:1.3.22")
-
-    // redis
-    api("org.springframework.boot:spring-boot-starter-data-redis-reactive")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage")
-    }
 }
--- game-server/build.gradle.kts
+++ game-server/build.gradle.kts
@@ -8,11 +8,21 @@ plugins {
 dependencies {
     implementation(project(":core"))
 
-    // oauth
+    // Logstash game
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+
+    // oauth lobby
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-    // database
+    // kafka infra
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("io.projectreactor.kafka:reactor-kafka:1.3.22")
+
+    // redis infra
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+
+    // database infra
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("com.github.jasync-sql:jasync-r2dbc-mysql:2.2.4")
 
