22cf0da3c1644e218d39c52650c3ce1c30bb7a48
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-03-16 21:01:20 +0900

    refactor - Matched, Waiting 상태 분기로 matchStatus 삭제

--- game-server/src/main/kotlin/game/server/lobby/dto/v1/response/MatchResponseDto.kt
+++ game-server/src/main/kotlin/game/server/lobby/dto/v1/response/MatchResponseDto.kt
@@ -2,19 +2,14 @@ package game.server.lobby.dto.v1.response
 
 import game.server.lobby.domain.match.MatchType
 
-sealed class MatchResponseDto(open val status: MatchStatus, open val matchType: MatchType)
+sealed class MatchResponseDto(open val matchType: MatchType)
 
 data class Matched(
     val matchId: String,
     val sessionIds: List<String>,
     override val matchType: MatchType
-) : MatchResponseDto(MatchStatus.MATCHED, matchType)
+) : MatchResponseDto(matchType)
 
 data class Waiting(
     override val matchType: MatchType
-) : MatchResponseDto(MatchStatus.WAITING, matchType)
-
-
-enum class MatchStatus {
-    MATCHED, WAITING
-}
+) : MatchResponseDto(matchType)
