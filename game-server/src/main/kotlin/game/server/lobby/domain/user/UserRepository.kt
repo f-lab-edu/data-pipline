e875b94c37d7e417fb98fad8dfd71af2094cbe49
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-03-17 14:18:09 +0900

    refactor - user repository core 모듈로 이동

--- game-server/src/main/kotlin/game/server/lobby/domain/user/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package game.server.lobby.domain.user
-
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-
-
-interface UserRepository : CoroutineCrudRepository<User, String> {
-    suspend fun save(user: User): User
-    suspend fun findByProviderId(providerId: String): User?
-}
