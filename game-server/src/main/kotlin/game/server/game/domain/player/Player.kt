6419d9123e3f55092673ac7b9c8e0cade3f87f79
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-03-18 17:42:34 +0900

    feat - player 별 소켓 및 세션id 가지고 있도록 수정 및 초기 맺은 소켓 정보로 플레이어 생성하도록 적용

--- game-server/src/main/kotlin/game/server/game/domain/player/Player.kt
+++ game-server/src/main/kotlin/game/server/game/domain/player/Player.kt
@@ -3,11 +3,12 @@ package game.server.game.domain.player
 import game.server.game.domain.vo.Position
 import game.server.game.handler.CANVAS_HEIGHT
 import game.server.game.handler.CANVAS_WIDTH
-import org.springframework.stereotype.Component
+import org.springframework.web.reactive.socket.WebSocketSession
 
-
-@Component
 class Player(
+    val sessionId: String,
+    val matchId: String,
+    val socket: WebSocketSession,
     val health: Int = 50,
     val damage: Int = 10,
     val speed: Int = 5
--- game-server/src/main/kotlin/game/server/game/service/MatchedEventService.kt
+++ game-server/src/main/kotlin/game/server/game/service/MatchedEventService.kt
@@ -2,6 +2,8 @@ package game.server.game.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.game.dto.v1.maching.Matched
+import game.server.game.domain.player.Player
+import game.server.game.session.PlayerManager
 import game.server.game.session.WebSocketSessionManager
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.joinAll
@@ -14,12 +16,21 @@ import reactor.core.publisher.Mono
 class MatchedEventService(
     private val sessionManager: WebSocketSessionManager,
     private val objectMapper: ObjectMapper,
+    private val playerManager: PlayerManager
 ) {
     suspend fun processMatchedEvent(matched: Matched) = coroutineScope {
         val message = objectMapper.writeValueAsString(matched)
 
         matched.sessionIds.mapNotNull { sessionKey ->
             sessionManager.getSession(sessionKey)
+                ?.also { webSocketSession ->
+                    val player = Player(
+                        sessionId = sessionKey,
+                        matchId = matched.matchId,
+                        socket = webSocketSession
+                    )
+                    playerManager.addPlayer(matched.matchId, player)
+                }
         }.map { webSocketSession ->
             launch {
                 webSocketSession.send(
