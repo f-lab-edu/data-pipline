9dfe44829cb226c1b628d8cefd9015c4ee24227e
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-03-18 17:36:22 +0900

    feat - 플레이어 위치 이벤트 발행 카프카 템플릿 기능 적용

--- core/src/main/kotlin/com/game/service/v1/matching/MatchEventPublisher.kt
+++ core/src/main/kotlin/com/game/service/v1/matching/MatchEventPublisher.kt
@@ -1,8 +1,10 @@
 package com.game.service.v1.matching
 
 import com.game.dto.v1.maching.Matched
+import com.game.dto.v1.move.PlayerMoved
 
 
 interface MatchEventPublisher {
     suspend fun publishMatchStart(matched: Matched)
+    suspend fun publishPlayerMovement(playerMoved: PlayerMoved)
 }
