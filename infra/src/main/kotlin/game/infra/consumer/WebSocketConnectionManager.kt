bf350457389ddbfa9f940eb9c687f2778e7076a2
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-03-28 22:20:45 +0900

    update - WebSocketConnectionManager 컨슈머 프로파일에서만 실행되도록 수정

--- infra/src/main/kotlin/game/infra/consumer/WebSocketConnectionManager.kt
+++ infra/src/main/kotlin/game/infra/consumer/WebSocketConnectionManager.kt
@@ -6,6 +6,7 @@ import com.game.dto.v1.maching.Matched
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.socket.WebSocketSession
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient
@@ -13,6 +14,7 @@ import reactor.core.publisher.Mono
 import java.net.URI
 import java.util.concurrent.ConcurrentHashMap
 
+@Profile("consumer-local | consumer-prod")
 @Import(ObjectConfig::class)
 @Component
 class WebSocketConnectionManager(
