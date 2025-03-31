d05b208367839b8f5de551467cafc8a489f9d000
Stark-Industries0417 <59994664+Stark-Industries0417@users.noreply.github.com>
2025-03-31 22:52:41 +0900

    update - consumer PlayerMoved 데이터에 seq 데이터 추가

--- core/src/main/kotlin/com/game/dto/v1/move/PlaerMoved.kt
+++ core/src/main/kotlin/com/game/dto/v1/move/PlaerMoved.kt
@@ -4,11 +4,11 @@ import com.game.dto.v1.maching.KafkaEvent
 
 data class PlayerMoved(
     override val eventType: String = "PLAYER_MOVED",
+    val seq: Int,
     val playerId: String,
     val matchId: String,
     val newPositionX: Int,
     val newPositionY: Int,
     val receivers: List<String>,
-    val timestamp: Long = System.currentTimeMillis()
+    val timestamp: Long
 ) : KafkaEvent
-
